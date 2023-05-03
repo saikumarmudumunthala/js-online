f5a0c840aef0af56d49ab33436eea8293bcafc10
saikumarmudumunthala <mudumunthalasaikumar04@gmail.com>
2023-05-04 01:51:42 +0530

    hai

---
+++ script.js
@@ -0,0 +1,21 @@
+//console.log
+
+console.log('sai kumar')
+
+//variables
+
+name = "sai kumar"
+console.log(name)
+
+sentence = "hlo how are u i hope u doing nice to see you"
+
+console.log(sentence)
+
+//variables
+
+var food =20;
+var tipPerstage = 0.2
+var tipAmount = food * tipPerstage
+
+console.log(tipAmount)
+
